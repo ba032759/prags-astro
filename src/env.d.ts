68a8c09f847775368770a9a27076ea0649f3e843
Tobias Jakubowitz <tobias.jakubowitz@superluminar.io>
2024-04-12 17:55:12 +0200

    add KV and endpoint

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,27 @@
 /// <reference types="astro/client" />
 
+// Calling it Env clashes with the @cloudflare type
+type AppEnv = {
+  // Bindings
+  DB: D1Database;
+  R2_CDN: R2Bucket;
+  // Secrets
+  AUTH_SECRET: string;
+  MY_KV: KVNamespace;
+};
+
 type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
 type ENV = {
   // replace `MY_KV` with your KV namespace
   MY_KV: KVNamespace;
 };
 
-// Depending on your adapter mode
-// use `AdvancedRuntime<ENV>` for advance runtime mode
-// use `DirectoryRuntime<ENV>` for directory runtime mode
+// // Depending on your adapter mode
+// // use `AdvancedRuntime<ENV>` for advance runtime mode
+// // use `DirectoryRuntime<ENV>` for directory runtime mode
 type Runtime = import("@astrojs/cloudflare").DirectoryRuntime<ENV>;
 declare namespace App {
-  interface Locals extends Runtime {}
+  interface Locals {
+    runtime: Runtime;
+  }
 }
--- src/pages/my-endpoint.ts
+++ src/pages/my-endpoint.ts
@@ -1,8 +1,14 @@
-// import type { APIContext } from "astro";
+import type { APIContext } from "astro";
 
-// export async function get({ locals }: APIContext) {
-//   // the type KVNamespace comes from the @cloudflare/workers-types package
-//   const { MY_KV } = locals.runtime.env;
+export async function get({ locals }: APIContext) {
+  // the type KVNamespace comes from the @cloudflare/workers-types package
+  const { MY_KV } = locals.runtime.env;
+
+  return MY_KV.get("key");
+}
+
+// export function GET(context: APIContext) {
+//   const { MY_KV }= context.locals.runtime.env;
 
 //   return MY_KV.get("key");
 // }
