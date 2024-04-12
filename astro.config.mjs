280c88515d5a4cac492eb8a62d26abdae61fc82c
Tobias Jakubowitz <tobias.jakubowitz@superluminar.io>
2024-04-12 17:20:45 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    mode: "directory",
+    imageService: "cloudflare",
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+});
